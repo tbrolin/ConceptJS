5232f3ba3f27e9ae473ed10176e8d6706c50e1cf
Tobbe Brolin <tobias.brolin@atex.com>
2014-05-22 15:25:12 +0200

    Added graph concept.

---
+++ src/con.concept.graph.js
@@ -0,0 +1,69 @@
+concept.define('graph', ['uuid'], function () {
+  var UUID = concept('uuid');
+
+  if (!this.Graph) {
+    var Graph = function () {
+      if (!(this instanceof Graph)) {
+        return new Graph();
+      }
+      this.size = 0;
+      this.nodes = {};
+      this.edges = {};
+    };
+
+    Graph.prototype.addNode = function (node) {
+      if (!node.hasOwnProperty('_nodeId') || !this.hasNode(node)) {
+        node._nodeId = UUID.generate();
+        this.nodes[node._nodeId] = node;
+        this.edges[node._nodeId] = [];
+      }
+    };
+
+    Graph.prototype.hasNode = function (node) {
+      return this.nodes[node._nodeId] ? true : false;
+    };
+
+    Graph.prototype.addEdge = function (from, to) {
+      if (!this.hasEdge(from, to)) {
+        this.edges[from._nodeId].push(to);
+      }
+    };
+
+    Graph.prototype.hasEdge = function (from, to) {
+      var index, edges = this.edges[from._nodeId];
+      for (index = 0; index < edges.length; index++) {
+        if (edges[index]._nodeId === to._nodeId) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    Graph.prototype.getAdjacencies = function (node) {
+      return this.edges[node._nodeId];
+    };
+
+    Graph.prototype.getPath = function (root, target, where) {
+      var path = [], queue = [], adjacs = [], index = 0, test;
+      where = where || function (a, b) { return a._nodeId === b._nodeId; };
+      queue.push(root);
+      while (queue.length > 0) {
+        test = queue.shift();
+        path.push(test);
+        if (where(test, target)) {
+          return path;
+        }
+        adjacs = graph.getAdjacencies(test);
+        if (adjacs.length === 0) {
+          path.pop();
+        }
+        for (index = 0; index < adjacs.length; index++) {
+          queue.push(adjacs[index]);
+        }
+      }
+    };
+
+    this.Graph = Graph;
+  }
+  return this.Graph;
+});
---
+++ test/con.test.concept.graph.js
@@ -0,0 +1,56 @@
+module('ConceptJS - graph', {
+  setup: function () {
+   var Graph = concept('graph'), g;
+   this.g = new Graph();
+   g = this.g;
+   console.log('graph', g);
+   this.nodes = [{ name: 'A' }, { name: 'B' },{ name: 'C' }, { name: 'D' }, { name: 'E' }];
+   this.nodes.forEach(function (node) {
+     g.addNode(node);
+   })
+  }
+});
+
+test('Graphs are graphs', function () {
+  var Graph = concept('graph');
+  var g = new Graph();
+  var gr = Graph();
+  ok(g instanceof Graph, 'Its a Graph!');
+  ok(gr instanceof Graph, 'Its a Graph!');
+});
+
+test('Added nodes', function () {
+  var g = this.g;
+  ok(g.size === 5, 'Graph has five nodes in it.');
+});
+
+test('Graph has nodes', function () {
+  var g = this.g;
+  ok(g.hasNode(this.nodes[2]), 'Graph has node "' + this.nodes[2].name + '".');
+});
+
+test('Create tree structure', function () {
+  var g = this.g, ns = this.nodes;
+
+  g.addEdge(ns[0], ns[1]);
+  g.addEdge(ns[0], ns[2]);
+  g.addEdge(ns[2], ns[3]);
+  g.addEdge(ns[2], ns[4]);
+
+  ok(g.edges[ns[0]._nodeId].length === 2, 'Node A has two edges');
+  ok(g.edges[ns[2]._nodeId].length === 2, 'Node C has two edges');
+  ok(g.hasEdge(ns[0], ns[2]), 'Node A has edge to node C');
+  ok(g.hasEdge(ns[2], ns[4]), 'Node C has edge to node E');
+});
+
+test('Get adjacencies', function () {
+  var g = this.g, ns = this.nodes;
+  g.addEdge(ns[0], ns[1]);
+  g.addEdge(ns[0], ns[2]);
+  g.addEdge(ns[2], ns[3]);
+  g.addEdge(ns[2], ns[4]);
+  var adjs = g.getAdjacencies(ns[0]);
+  console.log(adjs);
+  ok(adjs.length === 2, 'Node A has two adjacencies');
+  ok(adjs[0]._nodeId >= 0, 'Adjacency seems to be a node.');
+});
