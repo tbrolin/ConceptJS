6b7fd629230011b0072ae4c98e6546e3bf1fd11b
Tobbe Brolin <tobias.brolin@atex.com>
2015-02-05 10:22:06 +0100

    Added a sha1 concept.

---
+++ test/con.test.concept.sha1.js
@@ -0,0 +1,12 @@
+module('SHA1');
+
+test('Get SHA1', 1, function () {
+  var sha1 = concept('SHA1');
+  ok(sha1.digest, 'Digest function exist.');
+});
+
+test('Sha1 digest', function () {
+  var hash = '0a4d55a8d778e5022fab701977c5d840bbc486d0';
+  var sha1 = concept('SHA1');
+  ok (sha1.digest('Hello World')===hash, 'Digested "Hello World"');
+});
