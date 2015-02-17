b15fc09260c71493f07256b4137f864519afd4f7
Tobbe Brolin <tobias.brolin@atex.com>
2015-02-17 17:15:13 +0100

    Some test change.

--- test/con.test.concept.sha1.js
+++ test/con.test.concept.sha1.js
@@ -8,5 +8,5 @@ test('Get SHA1', 1, function () {
 test('Sha1 digest', function () {
   var hash = '0a4d55a8d778e5022fab701977c5d840bbc486d0';
   var sha1 = concept('SHA1');
-  ok (sha1.digest('Hello World')===hash, 'Digested "Hello World"');
+  ok (sha1.digest('Hello World') === hash, 'Digested "Hello World"');
 });
